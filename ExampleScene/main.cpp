b3595da228c5e06eb93aeaee4c1bd30285f5dd86
Mark <NIL>
2016-02-04 16:32:59 +0000

    Some small changes

--- ExampleScene/main.cpp
+++ ExampleScene/main.cpp
@@ -1,14 +1,26 @@
 #include "Scene.h"
 
 int main() {
-	// Create new MGLRenderer
-	Scene* scene = new Scene();
-	
-	while (!scene->ShouldClose()) {
-		scene->RenderScene();
+
+	int list[10];
+	int* p;
+
+	p = list;
+	for (int x = 1; x <= 10; x++) {
+		*(p) = x;
+		p++;
+		std::cout << &p << std::endl;
 	}
+
+
+	// Create new MGLRenderer
+	//Scene* scene = new Scene();
 	
-	delete scene;
+	//while (!scene->ShouldClose()) {
+		//scene->RenderScene();
+	//}
+	
+	//delete scene;
 
 	//_CrtDumpMemoryLeaks();
 	return 0;

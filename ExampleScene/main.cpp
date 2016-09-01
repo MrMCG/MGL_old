3c63cf8b3fcd5915d6e829154d18109cf4a0510a
ZeGoose <mark.gentles@hotmail.co.uk>
2016-09-01 14:28:06 +0100

    Updated to VS2015 + new glfw3 lib

--- ExampleScene/main.cpp
+++ ExampleScene/main.cpp
@@ -2,25 +2,14 @@
 
 int main() {
 
-	int list[10];
-	int* p;
-
-	p = list;
-	for (int x = 1; x <= 10; x++) {
-		*(p) = x;
-		p++;
-		std::cout << &p << std::endl;
-	}
-
-
 	// Create new MGLRenderer
-	//Scene* scene = new Scene();
+	Scene* scene = new Scene();
 	
-	//while (!scene->ShouldClose()) {
-		//scene->RenderScene();
-	//}
+	while (!scene->ShouldClose()) {
+		scene->RenderScene();
+	}
 	
-	//delete scene;
+	delete scene;
 
 	//_CrtDumpMemoryLeaks();
 	return 0;
